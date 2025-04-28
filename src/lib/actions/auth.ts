84609aa9ff234d3c6196a7609f5065350e947b3d
Abdullah al Mahmud Talha <abdullahalmahmudtalha@192.168.1.6>
2025-04-29 02:28:26 +0600

    implement qstash workflows for user onboarding

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -8,6 +8,8 @@ import { eq } from 'drizzle-orm';
 import { headers } from 'next/headers';
 import ratelimit from '../ratelimit';
 import { redirect } from 'next/navigation';
+import { workflowClient } from '../workflow';
+import config from '../config';
 
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, 'email' | 'password'>
@@ -78,6 +80,14 @@ export const signUp = async (params: AuthCredentials) => {
       password: hashedPassword,
       universityId,
       universityCard,
+    });
+
+    await workflowClient.trigger({
+      url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
+      body: {
+        email,
+        fullName,
+      },
     });
 
     await signInWithCredentials({ email, password });
